c84df70c6e1eed7c2cd876a4ad3c4e7da1002985
Aytug <aaytugozkaya@gmail.com>
2022-12-08 15:14:00 +0300

    Revert "Delete BackUp directory"
    This reverts commit 6085c974350032fbe238fe44c43d66a371265341.

---
+++ BackUp/Models/WebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackUp.Models
+{
+	public class WebAppContext :DbContext
+	{
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=Crud2;User=SA;Password=MyPassword123#;Trusted_Connection=false;Encrypt=false;");
+        }
+
+    }
+}
+
