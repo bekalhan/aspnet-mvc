e5745597fd3b026480d93741c632f3efb282d832
root07 <77151270+bekalhan@users.noreply.github.com>
2022-12-14 16:17:39 -0800

    comment functionality added

--- WP/Controllers/HomeController.cs
+++ WP/Controllers/HomeController.cs
@@ -25,32 +25,64 @@ public class HomeController : Controller
         var allProducts = (from y in _context.Products select y).ToList();
         return View(allProducts);
     }
-    public IActionResult CommentSection(int productId, string context)
-    {
-        Comment comment = new Comment();
-        comment.ProductId = productId;
-        comment.commentDate = DateTime.Now;
-        comment.commentString = context;
-        _context.Add(comment);
-        _context.SaveChanges();
-        return RedirectToAction("Index");
-    }
 
-    
-   
-    public async Task<IActionResult> DetailedProducts(int id)
-    {
-        ShoppingCart cartObj = new()
-        {
-            ProductId = id,
-            Product = await _context.Products.Include(k => k.Category).FirstOrDefaultAsync(m => m.ID == id)
-        };
-       
-
-        return View(cartObj);
-    }
+    /*  public IActionResult CommentSection(int productId, string context)
+      {
+          Comment comment = new Comment();
+          comment.ProductId = productId;
+          comment.commentDate = DateTime.Now;
+          comment.commentString = context;
+          _context.Add(comment);
+          _context.SaveChanges();
+          return RedirectToAction("Index");
+      }*/
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize]
+    public IActionResult AddCommentToProduct(Comment comment, string context)
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        comment.UserId = claim.Value;
+
+        _context.Comments.Add(comment);
+        _context.SaveChanges();
+
+        TempData["success"] = "You has been added a comment for this product";
+
+        return RedirectToAction("Index");
+
+    }
+
+    public async Task<IActionResult> DetailedProducts(int id)
+    {
+        CartCommentProduct cc = new CartCommentProduct();
+        var shoppingobje = cc.ShoppingCart;
+        var commentobje = cc.Comment;
+        var productobje = cc.Product;
+
+        shoppingobje = new()
+        {
+            ProductId = id,
+            Product = await _context.Products.Include(k => k.Category).FirstOrDefaultAsync(m => m.ID == id),
+        };
+        commentobje = new()
+        {
+            ProductId = id,
+        };
+        productobje = new()
+        {
+            ProductComments = await _context.Comments.Where(a => a.ProductId == id).Include(p => p.User).ToListAsync(),
+        };
+        cc.ShoppingCart = shoppingobje;
+        cc.Comment = commentobje;
+        cc.Product = productobje;
+
+        return View(cc);
+    }
+
+  /*  [HttpPost]
     [ValidateAntiForgeryToken]
     [Authorize]
     public async Task<IActionResult> DetailedProducts(ShoppingCart shoppingCart)
@@ -67,7 +99,7 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
-    }
+    }*/
 
     
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ WP/Models/CartCommentProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace WP.Models
+{
+	public class CartCommentProduct
+	{
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public Comment Comment { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
+
