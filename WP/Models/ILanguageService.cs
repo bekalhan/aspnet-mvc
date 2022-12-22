5facf104d343fde12f45dea119665df24202e715
Aytug <aaytugozkaya@gmail.com>
2022-12-22 17:10:40 +0300

    LanguageServices

---
+++ WP/Models/ILanguageService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace WP.Models
+{
+    public interface ILanguageService
+    {
+        public LocalizedString Getkey(string key);
+    }
+}
---
+++ WP/Models/LanguageService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace WP.Models
+{
+    public class LanguageService:ILanguageService
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public LanguageService(IStringLocalizerFactory factory)
+        {
+            var type = typeof(ShareResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _localizer = factory.Create("SharedResource", assemblyName.Name);
+        }
+
+        public LocalizedString Getkey(string key)
+        {
+            return _localizer[key];
+        }
+
+    }
+}
